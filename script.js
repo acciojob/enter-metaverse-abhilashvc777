6428c68dec145ad905875963eb3f347ea618e496
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-19 02:04:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let body = document.querySelector("body")
+let p = document.querySelector("p")
+const btn = document.querySelector("button")
+btn.addEventListener("click",(e)=>{
+	const h1 = document.createElement("h1")
+	h1.innerHTML = "Entered Metaverse"
+	body.replaceChild(h1,p)
+	// console.log(body)
+})
+// console.log(body) 
+
