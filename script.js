a17b95362c46f295eced8901ce691ff1a334cc76
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-19 02:34:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //your JS code here. If required.
-let body = document.querySelector("body")
+// let body = document.querySelector("body")
 let p = document.querySelector("#status")
 const btn = document.querySelector("#enterBtn")
-btn.addEventListener("click",(e)=>{
-	const h1 = document.createElement("h1")
+btn.addEventListener("mouseover",(e)=>{
+	const h1 = document.createElement("h1") 
 	h1.innerHTML = "Entered Metaverse"
-	body.replaceChild(h1,p)
+	p.parentNode.replaceChild(h1,p)
 	// console.log(body)
-})
-// console.log(body) 
+}) 
+
 
