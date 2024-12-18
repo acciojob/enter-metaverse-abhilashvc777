ab09818a9f48fee038f48c8ca31334b3b106851d
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-19 02:08:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 let body = document.querySelector("body")
-let p = document.querySelector("p")
-const btn = document.querySelector("button")
+let p = document.querySelector("#status")
+const btn = document.querySelector("#enterBtn")
 btn.addEventListener("click",(e)=>{
 	const h1 = document.createElement("h1")
 	h1.innerHTML = "Entered Metaverse"
